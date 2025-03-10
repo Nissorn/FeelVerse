a5b254fc08844bdabc893aa5703dbaad1d50d5db
stamppppps <Stamp26.2547@gmail.com>
2025-03-10 22:07:07 +0700

    token complete

--- server/routes/auth.js
+++ server/routes/auth.js
@@ -4,12 +4,29 @@ const router = express.Router();
 
 const { register,login, listuser, edituser, deleteuser } = require('../src/controllers/auth');
 
+//middleware
+const { auth } =require('../src/middleware/auth');
+
+
+
+
 //enpoint
 router.post("/register",register);
 
 
 router.post("/login",login);
 
+router.get('/1',auth,(req,res) =>{
+    res.send('hello 1')
+
+
+})
+
+router.get('/2',auth,(req,res) =>{
+    res.send('hello 2')
+
+
+})
 
 
 router.get('/auth',listuser);
