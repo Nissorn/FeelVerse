6e519d5e6ba563edec33b90973c82ac23f6f5622
stamppppps <Stamp26.2547@gmail.com>
2025-03-11 02:32:13 +0700

    auth.js in client

--- client/src/functions/auth.js
+++ client/src/functions/auth.js
@@ -1,5 +1,14 @@
-import axios from 'axios'
+import axios from "axios";
 
-export const register = async(formData) =>
-    await axios.post(import.meta.env.VITE_REACT_APP_API + "/register", formData);
+const API_URL = "http://localhost:5000/api"; // ต้องมี /api ถ้า Backend ใช้ prefix นี้
 
+export const register = async (formData) => {
+    console.log("API NOW")
+    // try {
+    //     const response = await axios.post(`${API_URL}/register`, formData);
+    //     return response.data;
+    // } catch (error) {
+    //     console.error("Registration error:", error.response?.data || error.message);
+    //     throw error;
+    // }
+};
